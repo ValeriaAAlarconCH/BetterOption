4d7ff7c6f02ac5414b191262fd91b7cc89ab4528
ValeriaAAlarconCH <u20221d218@upc.edu.pe>
2025-05-06 21:04:09 -0500

    base

---
+++ src/main/java/pe/edu/upc/grupo1_betteroption/Grupo1BetteroptionApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.grupo1_betteroption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1BetteroptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo1BetteroptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/grupo1_betteroption/Grupo1BetteroptionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.grupo1_betteroption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo1BetteroptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
