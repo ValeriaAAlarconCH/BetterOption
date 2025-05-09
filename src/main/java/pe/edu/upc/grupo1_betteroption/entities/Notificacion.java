380b6aa6d9bafcc1a8593b8303e828321012124f
ValeriaAAlarconCH <u20221d218@upc.edu.pe>
2025-05-09 16:34:47 -0500

    Tabla Notificacion

---
+++ src/main/java/pe/edu/upc/grupo1_betteroption/entities/Notificacion.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.grupo1_betteroption.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Notificacion")
+public class Notificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_notificacion", nullable = false)
+    private Long id_notificacion;
+
+    @Column(name = "mensaje", nullable = false, length = 100)
+    private String mensaje;
+
+    @Column(name = "tipo", nullable = false, length = 100)
+    private String tipo;
+
+    @Column(name = "fecha_envio", nullable = false)
+    private LocalDate fechaEnvio;
+
+    @Column(name = "leido", nullable = false)
+    private Integer leido;
+
+}
