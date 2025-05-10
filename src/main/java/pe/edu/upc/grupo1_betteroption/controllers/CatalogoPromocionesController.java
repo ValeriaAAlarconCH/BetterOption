9d3d2d7e863097f1df642717175b35b5d16a6b5f
ValeriaAAlarconCH <u20221d218@upc.edu.pe>
2025-05-09 19:31:14 -0500

    Tabla CatalogoPromociones

---
+++ src/main/java/pe/edu/upc/grupo1_betteroption/controllers/CatalogoPromocionesController.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.grupo1_betteroption.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.grupo1_betteroption.dtos.CatalogoPromocionesDto;
+import pe.edu.upc.grupo1_betteroption.services.CatalogoPromocionesService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalogospromociones")
+public class CatalogoPromocionesController {
+    @Autowired
+    private CatalogoPromocionesService catalogopromocionesservice;
+
+    @PostMapping("/registrar")
+    public ResponseEntity<CatalogoPromocionesDto> catalogopromociones(@RequestBody CatalogoPromocionesDto catalogopromocionesdto) {
+        return ResponseEntity.ok(catalogopromocionesservice.grabarCatalogoPromociones(catalogopromocionesdto));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<CatalogoPromocionesDto>> getCatalogosPromociones() {
+        return ResponseEntity.ok(catalogopromocionesservice.getCatalogosPromociones());
+    }
+
+}
---
+++ src/main/java/pe/edu/upc/grupo1_betteroption/repositories/CatalogoPromocionesRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.grupo1_betteroption.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.grupo1_betteroption.entities.CatalogoPromociones;
+
+public interface CatalogoPromocionesRepository extends JpaRepository<CatalogoPromociones, Long> {
+}
